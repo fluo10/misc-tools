70b59d55d705f9bf4c3fcca7d815002859a413e7
fluo10 <fluo10.dev@fireturtle.net>
2021-05-28 08:00:24 +0000

    Add variable to handle arg

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,28 @@
 use std::env;
 
+//fn readline(&mut src: String, &mut dst: String)-> Result() {
+//    println!("Please input source directory.");
+//    io::stdin()
+//        .read_line(&mut src)
+//        .expect("Failed to read line");
+//    println!("Please input source directory.");
+//                
+//    io::stdin()
+//        .read_line(&mut dst)
+//        .expect("Failed to read line");
+//}
+fn validate_args() {
+
+}
+
 fn main() {
-        let args: Vec<String> = env::args().collect();
-        println!("{:?}", args);
+    //let &mut src;
+    //let &mut dst;
+
+    let args: Vec<String> = env::args().collect();
+    let src = &args[1];
+    let dst = &args[2];
+    
+    println!("src: {}", src);
+    println!("dst: {}", dst);
 }
